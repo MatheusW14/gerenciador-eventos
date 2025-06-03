baae5fd585000f665703c84c9274d21b23310bf9
MatheusW14 <matheuscostamendes@hotmail.com>
2025-06-03 16:48:06 -0300

    adiciona função para cadastrar participantes

--- participantes.py
+++ participantes.py
@@ -1,6 +1,22 @@
 from utils import obter_entrada
 import random
 
+participantes = []
+
 
 def gerar_id_participante():
     return f"P-{random.randint(1000,9999)}"
+
+
+def cadastrar_participante():
+    id_participante = gerar_id_participante()
+
+    dados = {
+        "nome": obter_entrada("Nome Completo: "),
+        "email": obter_entrada("E-mail: "),
+        "preferencias": obter_entrada(
+            "Preferências temáticas (separadas por vírgula): "
+        ).split(","),
+    }
+
+    participantes.append({"id": id_participante, **dados})
